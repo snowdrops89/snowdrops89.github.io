c8f0f27c7d03b428f2d8c96efbbda930d1139def
雪乃☆雫 <59927104+snowdrops89@users.noreply.github.com>
2021-03-26 14:23:46 +0900

    Maplatのテスト

---
+++ Maplat_test/test_core/app.js
@@ -0,0 +1,23 @@
+﻿var option = {
+  appid: "Maplat_test_core",
+};
+
+Maplat.createObject(option).then(function(app) {
+  const viewpoint = {
+    zoom: 3  //古地図倍率
+  }
+  const ratio = 20;  //透明度
+
+  window.setTimeout(function(){
+    app.setViewpoint(viewpoint);
+    app.setTransparency(ratio);
+  }, 1);
+
+//ボタンで地図を切り替え
+  document.getElementById("osm").addEventListener("click", function(e) {
+    app.changeMap("osm");
+  });
+  document.getElementById("kochizu").addEventListener("click", function(e) {
+    app.changeMap("goryokaku");
+  });
+});
